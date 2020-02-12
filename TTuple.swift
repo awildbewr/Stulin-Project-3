06b9691d033243e1714d5ed2cbf9b6e6fe821427
Nicholas White <nicholaswhitepersonal@gmail.com>
2020-02-12 20:21:01 +0000

    added some of initial code to start with.

---
+++ TTuple.swift
@@ -0,0 +1,34 @@
+enum Direction {
+  case left
+  case right
+}
+
+struct TTuple: CustomStringConvertible {
+  let currentState: Int
+  let inputChar: Character
+  let newState: Int
+  let outputChar: Character
+  let direction: Direction
+
+  static func makeKey(state: Int, inputChar: Character) -> String {
+    return "placeholder"
+  }
+  func getKey() -> String {
+    return "placeholder"
+  }
+  init(currentState: Int, inputChar: Character, newState: Int, outputChar: Character, direction: Direction) {
+    self.currentState = currentState
+    self.inputChar = inputChar
+    self.newState = newState
+    self.outputChar = outputChar
+    self.direction = direction
+  }
+  var description: String
+  //Method needed for extra credit
+  init(tupleString: String) {
+
+  }
+  func nakedTuple() -> String {
+    return "placeholder"
+  }
+}
---
+++ main.swift
@@ -0,0 +1,1 @@
+simpleTuringRun()
